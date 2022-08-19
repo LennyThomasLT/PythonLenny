8893bb2f089f8a71d73c45bedf1ad138a8e0d69c
Lenny Thomas <105914077+LennyThomasLT@users.noreply.github.com>
2022-08-19 16:21:53 +0700

    Create CS01-9.py

---
+++ CS01-9.py
@@ -0,0 +1,9 @@
+one = int(input("Your number 1 = "))
+two = int(input("Your number 2 = "))
+three = int(input("Your number 3 = "))
+
+if one>two and one<three:
+    print("conditons is true")
+else:
+    print("ERROR")
+
